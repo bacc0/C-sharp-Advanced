9b04ef4eedce9b2942497b3ebdc8d168dabcdfab
Veselin Trayanov <bacco7@me.com>
2018-07-24 15:25:38 +0100

    03

---
+++ Functional Programming/03_Count_Uppercase_Words/03_Count_Uppercase_Words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Count_Uppercase_Words
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
