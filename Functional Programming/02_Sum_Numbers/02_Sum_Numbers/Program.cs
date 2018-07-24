81013236e348680ef18873a5c9f4e07c1692f8d9
Veselin Trayanov <bacco7@me.com>
2018-07-24 14:40:26 +0100

    sum

---
+++ Functional Programming/02_Sum_Numbers/02_Sum_Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _Sum_Numbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                               .Split(new char[] { ',', ' ' }
+                                      , StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+            var sum = 0;
+            
+            foreach (var n in input)
+            {
+                sum += n;
+            }
+            Console.WriteLine(input.Length + "\n" + sum);
+        }
+    }
+}
