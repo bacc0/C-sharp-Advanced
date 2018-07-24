764a8f93ad4b924dd103fdfaaf6c214eaa0237c4
Veselin Trayanov <bacco7@me.com>
2018-07-24 14:17:54 +0100

    new

---
+++ Functional Programming/01_Sort_Even_Numbers/01_Sort_Even_Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _Sort_Even_Numbers
+{
+    class MainClass
+    {
+        private const int V = 2;
+
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                               .Split(", ".ToCharArray()
+                                      ,StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .Where(n => n % 2 == 0)
+                               .OrderBy(x => x)
+                               .ToArray();
+            
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
