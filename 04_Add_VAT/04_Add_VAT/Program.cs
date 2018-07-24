9fa4d35610e117c5b95cb02ed9d3e2831566a94f
Veselin Trayanov <bacco7@me.com>
2018-07-24 16:24:36 +0100

    new

---
+++ 04_Add_VAT/04_Add_VAT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Add_VAT
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
