4770e61234b43310e5deae2cad596afe9affd4f7
Veselin Trayanov <bacco7@me.com>
2018-07-24 18:03:41 +0100

    1

--- Functional Programming/05_Filter_by_Age/05_Filter_by_Age/Program.cs
+++ Functional Programming/05_Filter_by_Age/05_Filter_by_Age/Program.cs
@@ -8,34 +8,61 @@ namespace _Filter_by_Age
     {
         public static void Main(string[] args)
         {
+            var peopleCount = int.Parse(Console.ReadLine());
 
-            var people = new Dictionary<string, int>();
+            var people = new Dictionary<string, int>(peopleCount);
 
-            var rot = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < rot; i++)
+            for (int i = 0; i < peopleCount; i++)
             {
                 var input = Console.ReadLine().Split(", ".ToCharArray()
-                                                     ,StringSplitOptions
+                                                     , StringSplitOptions
                                                      .RemoveEmptyEntries);
 
-                var person = input[0];
-                var years  = int.Parse(input[1]);
+                people.Add(input[0], int.Parse(input[1]));
 
-                if ( !people.ContainsKey(person))
-                {
-                    people.Add(person, years);
-                }
             }
-            var oldYoung = Console.ReadLine();
-            var age = int.Parse(Console.ReadLine());   
-            var name = Console.ReadLine();
+            var condition = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var format = Console.ReadLine();
 
-            if (true)
+            var filter = CreateFilter(condition, age);
+            var printer = CreatePrinter(format);
+            PrintPeople(people, filter, printer);
+
+
+            //people.Where(p => filter(p.Value))
+            //.ToList()
+            //.ForEach(printer);
+        }
+
+        private static void PrintPeople(Dictionary<string, int> people, Func<int, bool> filter, Action<KeyValuePair<string, int>> printer)
+        {
+            foreach (var p in people)
             {
-
+                if (filter(p.Value))
+                    printer(p);
             }
+        }
 
+        static Func<int, bool> CreateFilter(string condition, int age)
+        {
+            if (condition == "younger")
+                return x => x < age;      
+            
+            else
+                return x => x >= age;
+        }
+
+        static Action<KeyValuePair<string, int>> CreatePrinter(string format)
+        {
+            if (format == "name")
+                return x => Console.WriteLine(x.Key);
+            
+            else if (format == "age")
+                return x => Console.WriteLine(x.Value);
+            
+            else
+                return x => Console.WriteLine($"{x.Key} -{x.Value}");
         }
     }
 }
