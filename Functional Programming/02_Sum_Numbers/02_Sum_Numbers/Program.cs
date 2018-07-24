2b44047b0466219ee3ed88f39248ca31acea87c8
Veselin Trayanov <bacco7@me.com>
2018-07-24 15:25:06 +0100

    e

--- Functional Programming/02_Sum_Numbers/02_Sum_Numbers/Program.cs
+++ Functional Programming/02_Sum_Numbers/02_Sum_Numbers/Program.cs
@@ -7,19 +7,12 @@ namespace _Sum_Numbers
     {
         public static void Main(string[] args)
         {
-            //var sum = 0;
-
             var numbers = Console.ReadLine()
-                               .Split(new char[] { ',', ' ' }
+                                 .Split(new char[] { ',', ' ' }
                                       , StringSplitOptions.RemoveEmptyEntries)
-                               .Select(int.Parse)
-                               .ToList();
+                                 .Select(int.Parse)
+                                 .ToList();
             
-            //foreach (var n in input)
-            //{
-            //    sum += n;
-            //}
-
             Console.WriteLine(numbers.Count + "\n" + numbers.Sum());
         }
     }
