ba7a8c965cec8a4a68c90e5a664d2b4b2bc79fa9
Veselin Trayanov <bacco7@me.com>
2018-07-24 17:50:15 +0100

    w

--- Functional Programming/05_Filter_by_Age/05_Filter_by_Age/Program.cs
+++ Functional Programming/05_Filter_by_Age/05_Filter_by_Age/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _Filter_by_Age
 {
@@ -6,7 +8,34 @@ namespace _Filter_by_Age
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var people = new Dictionary<string, int>();
+
+            var rot = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rot; i++)
+            {
+                var input = Console.ReadLine().Split(", ".ToCharArray()
+                                                     ,StringSplitOptions
+                                                     .RemoveEmptyEntries);
+
+                var person = input[0];
+                var years  = int.Parse(input[1]);
+
+                if ( !people.ContainsKey(person))
+                {
+                    people.Add(person, years);
+                }
+            }
+            var oldYoung = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());   
+            var name = Console.ReadLine();
+
+            if (true)
+            {
+
+            }
+
         }
     }
 }
