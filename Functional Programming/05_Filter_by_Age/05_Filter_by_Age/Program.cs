edf7773a466adb7a5c3e8d9eb04388a30e765283
Veselin Trayanov <bacco7@me.com>
2018-07-24 17:07:08 +0100

    1

---
+++ Functional Programming/05_Filter_by_Age/05_Filter_by_Age/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Filter_by_Age
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
