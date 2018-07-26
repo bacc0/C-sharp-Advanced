5788e51889ee78c8787b300736b537d67914bbd9
Veselin Trayanov <bacco7@me.com>
2018-07-26 18:02:49 +0100

    05

---
+++ 01. Stacks and Queues/05_Hot_Potato/05_Hot_Potato/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Hot_Potato
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
