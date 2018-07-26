fdc594e185158514b0d9c69171f891b6e459a778
Veselin Trayanov <bacco7@me.com>
2018-07-26 19:28:51 +0100

    05

--- 01. Stacks and Queues/05_Hot_Potato/05_Hot_Potato/Program.cs
+++ 01. Stacks and Queues/05_Hot_Potato/05_Hot_Potato/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _Hot_Potato
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var children = Console.ReadLine().Split();
+            var tossLimit = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>(children);
+
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i < tossLimit; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                Console.WriteLine(queue.Dequeue());
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
         }
     }
 }
