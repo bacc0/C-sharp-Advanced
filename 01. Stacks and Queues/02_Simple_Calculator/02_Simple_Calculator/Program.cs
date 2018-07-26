1e850d4f2328f14b45a1b66647151df0dfc3658c
Veselin Trayanov <bacco7@me.com>
2018-07-26 15:11:08 +0100

    02

---
+++ 01. Stacks and Queues/02_Simple_Calculator/02_Simple_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Simple_Calculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
