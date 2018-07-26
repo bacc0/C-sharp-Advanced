f1d3cda837607bd24302d774bd03c3d50f8a86f4
Veselin Trayanov <bacco7@me.com>
2018-07-26 16:55:49 +0100

    02

--- 01. Stacks and Queues/02_Simple_Calculator/02_Simple_Calculator/Program.cs
+++ 01. Stacks and Queues/02_Simple_Calculator/02_Simple_Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _Simple_Calculator
 {
@@ -6,7 +7,34 @@ namespace _Simple_Calculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var elements = Console.ReadLine().Split();
+
+            var stack = new Stack<string>();
+
+            for (int counter = elements.Length - 1; counter >= 0; counter--)
+            {
+                stack.Push(elements[counter]);
+            }
+
+            while (stack.Count > 1)
+            {
+                var leftNum = int.Parse(stack.Pop());
+                var operation = stack.Pop();
+                var rightNum = int.Parse(stack.Pop());
+
+                switch (operation)
+                {
+                    case "+":
+                        stack.Push((leftNum + rightNum)
+                                   .ToString());
+                        break;
+                    case "-":
+                        stack.Push((leftNum - rightNum)
+                                   .ToString());
+                        break;
+                }
+            }
+            Console.WriteLine(stack.Pop());
         }
     }
 }
