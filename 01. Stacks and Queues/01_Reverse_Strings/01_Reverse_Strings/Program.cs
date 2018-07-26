bc932498a77a5bdd87ed87f6df211fff02deb582
Veselin Trayanov <bacco7@me.com>
2018-07-26 14:34:42 +0100

    01

---
+++ 01. Stacks and Queues/01_Reverse_Strings/01_Reverse_Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Reverse_Strings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
