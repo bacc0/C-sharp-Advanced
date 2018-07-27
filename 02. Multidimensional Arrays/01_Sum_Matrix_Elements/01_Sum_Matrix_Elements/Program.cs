5ba2a69d5d961169d4533b4f731df1bb0b19677e
Veselin Trayanov <bacco7@me.com>
2018-07-27 14:24:39 +0100

    01

---
+++ 02. Multidimensional Arrays/01_Sum_Matrix_Elements/01_Sum_Matrix_Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Sum_Matrix_Elements
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
